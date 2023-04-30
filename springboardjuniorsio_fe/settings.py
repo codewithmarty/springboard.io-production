ca3446891bf4590f73d08eeb36b298959a129076
Martin Nicola <lehitutoring@gmail.com>
2023-04-30 09:05:13 -0600

    deploy: made changes to requirements.txt

--- springboardjuniorsio_fe/settings.py
+++ springboardjuniorsio_fe/settings.py
@@ -114,7 +114,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend', 'dist')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'dist', 'static')]
 
